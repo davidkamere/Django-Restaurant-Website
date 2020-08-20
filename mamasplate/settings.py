9a669808d25569972785590af0506d81eebe6c58
davidkamere <54814346+davidkamere@users.noreply.github.com>
2020-08-20 15:59:12 +0300

    User views + UI/UX changes

--- mamasplate/settings.py
+++ mamasplate/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import django_heroku
 import os
-
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
