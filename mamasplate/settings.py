96a10deca90a6466e35e8c4bd99693dc3c69cc62
davidkamere <54814346+davidkamere@users.noreply.github.com>
2020-08-28 13:54:37 +0300

    S3 changes

--- mamasplate/settings.py
+++ mamasplate/settings.py
@@ -131,12 +131,12 @@ USE_S3 = 'TRUE'
 
 if USE_S3 == 'TRUE':
     # aws settings
-    AWS_S3_REGION_NAME = 'eu-east-2'
+    AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = 'AKIAV5TJQJ54OYI6ERMS'
     AWS_SECRET_ACCESS_KEY = '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'
     AWS_STORAGE_BUCKET_NAME = 'mamasplate'
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.us-east-2.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.c.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATIC_LOCATION = 'static'
